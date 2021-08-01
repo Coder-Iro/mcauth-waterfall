e884366c941586acc6b62577bf4fbc2a823e28ac
Patrick Choe <mailpatrickkr@gmail.com>
2021-08-01 12:17:35 +0900

    :construction: setup kotlin env

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    `java-library`
+    kotlin("jvm") version "1.5.21"
 }
 
 group = "io.mrsmc"
@@ -11,7 +11,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.github.waterfallmc:waterfall-api:1.17-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.github.waterfallmc:waterfall-api:1.17-R0.1-SNAPSHOT")
 
     implementation("com.j256.two-factor-auth:two-factor-auth:1.3")
     implementation("redis.clients:jedis:3.6.3")
